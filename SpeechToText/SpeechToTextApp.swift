0ba1ad74b686002b2f71fb92629a645feb0e2ca5
Jesus Antonio Cruz Suarez <jesus.cruz@alternova.com>
2024-10-10 17:09:24 -0500

    first commit

---
+++ SpeechToText/SpeechToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextApp.swift
+//  SpeechToText
+//
+//  Created by Jesus Cruz Suárez on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeechToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
